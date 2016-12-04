a8d854e61430a4b98ac0dc52e0bac514d4107b86
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-04 15:07:44 +0800

    phone number cleaning

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -22,6 +22,27 @@ def save_thank_you_letters(id,form_letter)
 	end
 end
 
+def clean_phonenumber(phone_number)
+	current_phonenum = phone_number.to_s.gsub(/[()-\.]|[\s]/,'')
+	current_phone_num_length = current_phonenum.length
+
+	if current_phone_num_length < 10
+		"BAD NUMBER"
+	elsif current_phone_num_length == 11
+		if current_phonenum[0] == "1"
+			current_phonenum[1..-1]
+		else 
+			"BAD NUMBER"
+		end
+	elsif current_phone_num_length > 11
+		"BAD NUMBER"
+	elsif current_phone_num_length == 10
+		current_phonenum
+	else
+		"BAD NUMBER"
+	end
+end
+
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
@@ -34,11 +55,12 @@ contents.each do |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
 	legislators = legislators_by_zipcode(zipcode)
+	phone_number = clean_phonenumber(row[:homephone])
 
 	form_letter = erb_template.result(binding)
 
 	save_thank_you_letters(id,form_letter)
-	#puts form_letter
+	#puts phone_number
 end
 
 
